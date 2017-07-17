bd598d1deab48b843b621d8a6cb28007a2ff5746
Abhishek Nagekar <abhisheknagekar@gmail.com>
2017-07-17 12:38:24 +0530

    .

--- quotes.js
+++ quotes.js
@@ -2,7 +2,8 @@ const quoteArray = [
   "Life is pain, Highness. Anyone who says differently is selling something.",
   "If you wish to make anything from scratch, you must first invent the Universe",
   "In reality, all models are wrong. The practical question is, how wrong does it have to be to not be useful",
-  "You shouldn't learn anything just because you have to. You must feel the need for it in your own Universe"
+  "You shouldn't learn anything just because you have to. You must feel the need for it in your own Universe",
+  "If you think it is hard to get humans to follow traffic laws, imagine convincing an astroid to move along an ellipse"
 ]
 
 exports.getQuote = function() {
