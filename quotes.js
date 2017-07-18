4556e79fc6dc922cdc6101b53a199b9b1fa3b4ce
Abhishek Nagekar <abhisheknagekar@gmail.com>
2017-07-18 13:50:47 +0530

    .

--- quotes.js
+++ quotes.js
@@ -3,7 +3,8 @@ const quoteArray = [
   "If you wish to make anything from scratch, you must first invent the Universe",
   "In reality, all models are wrong. The practical question is, how wrong does it have to be to not be useful",
   "You shouldn't learn anything just because you have to. You must feel the need for it in your own Universe",
-  "If you think it is hard to get humans to follow traffic laws, imagine convincing an astroid to move along an ellipse"
+  "If you think it is hard to get humans to follow traffic laws, imagine convincing an astroid to move along an ellipse - the grand design",
+  "Today you, tomorrow me. - reddit"
 ]
 
 exports.getQuote = function() {
